56b228e353d9e3f798ca3abf2c190501b37a2f26
Shahed Mehbub <shahed739@gmail.com>
2019-06-25 19:46:05 +0600

    init buildfiles

---
+++ build/precache-manifest.1ea2c5c459a03cfcdf65a4386208831d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a627e97fffa84a290568232e9c59ca83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67f1a1e9102568919130",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "8d05f91e23b9ca26542b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "67f1a1e9102568919130",
+    "url": "/static/js/2.239eaa96.chunk.js"
+  },
+  {
+    "revision": "8d05f91e23b9ca26542b",
+    "url": "/static/js/main.770f3ff6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
